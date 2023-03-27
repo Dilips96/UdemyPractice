414bf859eb6b36c9d88a7109752b329d257b83ee
Dilip <dil7978sahukar@gmail.com>
2023-03-27 12:51:50 +0530

    first commit

---
+++ src/Test/FirstAnnotation.java
@@ -0,0 +1,24 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class FirstAnnotation 
+{
+
+	@Test
+	public void Testscript1() 
+	{
+		System.out.println("This Includes the first test case");
+		
+		
+	}
+	
+	@Test
+	public void Testscript2() 
+	{
+		System.out.println("This is the Second Annotation");
+	}
+	
+	
+
+}
---
+++ src/Test/SecondAnno.java
@@ -0,0 +1,14 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class SecondAnno 
+{
+
+	@Test
+	public void Bus() 
+	{
+		System.out.println("This is The Second class ");
+	}
+	
+}
